bab7c9a9e7481720f05056834609213c0794ed87
Roman Shkromyda <shroman@ukr.net>
2018-09-26 01:28:33 +0300

    Templates: register and login added

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, flash, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 import requests
+
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -30,6 +32,7 @@ GoodreadsKey ="6FmDrXrBHdMm22IUXa5Kw"
 
 
 @app.route("/")
+@login_required
 def index():
     """Index page"""
     books = db.execute("SELECT * FROM books LIMIT 10;").fetchall()
@@ -45,13 +48,89 @@ def index():
     # return "Project 1: TODO" + res
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login"
+    """Log user in"""
 
-@app.route("/register")
+    # Forget any user_id
+    session.clear()
+
+    error = None
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            # return apology("must provide username", 403)
+            flash("must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("must provide password")
+
+        # Query database for username
+        # rows = db.execute("SELECT * FROM users WHERE username = :username",
+        #                   username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        # if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        #     return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        # return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return "Register"
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Missing username!")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("must provide password")
+
+        # Ensure password confirmation was submitted
+        elif not request.form.get("confirmation"):
+            flash("must provide password confirmation")
+
+        # Ensure password and confirmation match
+        elif not request.form.get("confirmation") == request.form.get("password"):
+            flash("passwords does not match")
+
+        # adduser = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+        #                      username=request.form.get("username"), hash=generate_password_hash(request.form.get("password")))
+        # if not adduser:
+        #     return apology("that username is taken", 400)
+        # # generate_password_hash    db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", username = request.form.get("username"), hash = hash)
+        #
+        # # Query database for username
+        # rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        #
+        # # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        # return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+    return render_template("register.html")
+    # return apology("TODO")
 
 @app.route("/search")
 def search():
