25d59fbafed431e35f1df7e6b26db707d50977e3
Roman Shkromyda <shroman@ukr.net>
2018-10-03 00:46:10 +0300

    cleaning files

--- env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export DATABASE_URL="postgres://reyktcizwqfkeh:3b566486c76c032b255767d78d00f7bc289ccc93283826f465c3825d8f1eff14@ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/ddgv5q820v7o4r"
